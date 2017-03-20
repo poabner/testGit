345de05aa7d395293906f6476ba2fe12900e2e21
pengdx <1056919591@qq.com>
2017-03-20 23:44:53 +0800

    输出word

--- src/main/java/testGit/Test.java
+++ src/main/java/testGit/Test.java
@@ -3,7 +3,8 @@ package testGit;
 public class Test {
 
 	public static void main(String[] args) {
-System.out.println("hello!!");
+		System.out.println("hello!!");
+		System.out.println("word!!");
 	}
 
 }
